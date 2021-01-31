612dfc68e6c61c80f1ab29508390ad5685d1358e
zerhaz <m.stancelewski@gmail.com>
2021-01-31 16:51:42 +0100

    Single-Relation / FIX /

--- Single-Relation/convertToNX.py
+++ Single-Relation/convertToNX.py
@@ -10,9 +10,7 @@ file = "CSD_48D496922CDF10DE06AC91967B5C7F9C.csv"
 def mappedRelation(_df):
     relationList = []
     for index, row in _df.iterrows():
-        first = row[0]
-        second = row[2]
-        tuple = (first, second)
+        tuple = (row[0], row[2])
         relationList.append(tuple)
     return relationList
 
@@ -20,8 +18,8 @@ def mappedRelation(_df):
 def showKawai(_df):
     print("Graph generation started...")
     G = nx.MultiGraph()
-    rel = mappedRelation(_df)
-    G.add_edges_from(rel)
+    relation = mappedRelation(_df)
+    G.add_edges_from(relation)
     pos = nx.kamada_kawai_layout(G)
     nx.draw_kamada_kawai(G, edge_color='grey')
     nx.draw_networkx_labels(G, pos)
@@ -32,18 +30,20 @@ def showKawai(_df):
 def showMultipartite(_df):
     print("Graph generation started...")
     G = nx.MultiGraph()
-    rel = mappedRelation(_df)
+    relation = mappedRelation(_df)
     listOfNodes1 = [seq[0] for seq in mappedRelation(_df)]
     listOfNodes2 = [seq[1] for seq in mappedRelation(_df)]
     G.add_nodes_from(listOfNodes1, layer=0)
     G.add_nodes_from(listOfNodes2, layer=1)
-    G.add_edges_from(rel)
+    G.add_edges_from(relation)
     pos = nx.multipartite_layout(G, subset_key="layer")
     nx.draw(G, pos, with_labels=True, edge_color="grey")
     print("Graph generated.")
     plt.show()
 
+
 if __name__ == '__main__':
     dfHasProductPrice = relationHasProductPrice(path, file)
-    #showKawai(dfHasProductPrice)
-    #showMultipartite(dfHasProductPrice)
+    ## DELETE ANY COMMENT BELOW TO SHOW GRAPH
+    # showKawai(dfHasProductPrice)
+    # showMultipartite(dfHasProductPrice)
--- Single-Relation/relations.py
+++ Single-Relation/relations.py
@@ -1,46 +1,38 @@
-import pandas as pd
-import csv
 from convertToDF import ConvertToDF
 
-path = "data-splitted/"
+path = "data-dev/"
 file = "CSD_48D496922CDF10DE06AC91967B5C7F9C.csv"
 
 __all__ = ["relationHasProductPrice", "relationHasAge", "relationHasGender", "relationHasBrand", "relationHasCountry", "relationHasTitle"]
 
 
 def relationHasProductPrice(_path, _file):
-    converter = ConvertToDF(_path, _file)
-    df = converter.listToDF('product_id', 'has_product_price', 'product_price')
+    df = ConvertToDF(_path, _file).listToDF('product_id', 'has_product_price', 'product_price')
     return df
 
 
 def relationHasAge(_path, _file):
-    converter = ConvertToDF(_path, _file)
-    df = converter.listToDF('product_id', 'has_age', 'product_age_group')
+    df = ConvertToDF(_path, _file).listToDF('product_id', 'has_age', 'product_age_group')
     return df
 
 
 def relationHasGender(_path, _file):
-    converter = ConvertToDF(_path, _file)
-    df = converter.listToDF('product_id', 'has_gender', 'product_gender')
+    df = ConvertToDF(_path, _file).listToDF('product_id', 'has_gender', 'product_gender')
     return df
 
 
 def relationHasBrand(_path, _file):
-    converter = ConvertToDF(_path, _file)
-    df = converter.listToDF('product_id', 'has_brand', 'product_brand')
+    df = ConvertToDF(_path, _file).listToDF('product_id', 'has_brand', 'product_brand')
     return df
 
 
 def relationHasCountry(_path, _file):
-    converter = ConvertToDF(_path, _file)
-    df = converter.listToDF('product_id', 'has_country', 'product_country')
+    df = ConvertToDF(_path, _file).listToDF('product_id', 'has_country', 'product_country')
     return df
 
 
 def relationHasTitle(_path, _file):
-    converter = ConvertToDF(_path, _file)
-    df = converter.listToDF('product_id', 'has_title', 'product_title')
+    df = ConvertToDF(_path, _file).listToDF('product_id', 'has_title', 'product_title')
     return df
 
 
