db7b9386e5c601c4c0b909b71c7cf49b44594bcc
zerhaz <m.stancelewski@gmail.com>
2021-01-31 15:15:42 +0100

    Kawai + Multipartite

--- convertToNX.py
+++ convertToNX.py
@@ -29,6 +29,21 @@ def showKawai(_df):
     plt.show()
 
 
+def showMultipartite(_df):
+    print("Graph generation started...")
+    G = nx.MultiGraph()
+    rel = mappedRelation(_df)
+    listOfNodes1 = [seq[0] for seq in mappedRelation(_df)]
+    listOfNodes2 = [seq[1] for seq in mappedRelation(_df)]
+    G.add_nodes_from(listOfNodes1, layer=0)
+    G.add_nodes_from(listOfNodes2, layer=1)
+    G.add_edges_from(rel)
+    pos = nx.multipartite_layout(G, subset_key="layer")
+    nx.draw(G, pos, with_labels=True, edge_color="grey")
+    print("Graph generated.")
+    plt.show()
+
 if __name__ == '__main__':
     dfHasProductPrice = relationHasProductPrice(path, file)
-    showKawai(dfHasProductPrice)
+    #showKawai(dfHasProductPrice)
+    #showMultipartite(dfHasProductPrice)
